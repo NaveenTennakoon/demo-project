dccf25d5096ee75189e7331a4aea55801f39b162
NaveenTennakoon <naveentennakoon27@gmail.com>
2020-10-16 16:46:24 +0530

    use hooks and remove classes

--- src/views/Home_Page.js
+++ src/views/Home_Page.js
@@ -1,46 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { withRouter } from "react-router";
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            islogout: false
-        };
-    }
+const Home = () => {
+    const[logout, setLogout] = useState(false);
 
-    signOut = () => {
+    const signOut = () => {
         localStorage.clear();
-        this.setState({
-            islogout: true
-        });
+        setLogout(true);
     };
 
-    render() {
-        if (this.state.islogout) {
-            return <Redirect to="/login" />;
-        }
-        return (
-        <Container component="main" maxWidth="xs">
-            <Typography component="h1" variant="h5">
-                Welcome {localStorage.getItem("username")}
-            </Typography>
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                onClick={this.signOut}
-            >
-                Sign Out
-            </Button>
-        </Container>
-        );
+    if (logout) {
+        return <Redirect to="/login" />;
     }
+    return (
+    <Container component="main" maxWidth="xs">
+        <Typography component="h1" variant="h5">
+            Welcome {localStorage.getItem("username")}
+        </Typography>
+        <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={signOut}
+        >
+            Sign Out
+        </Button>
+    </Container>
+    );
 }
  
 export default withRouter(Home);
--- src/views/Login_Page.js
+++ src/views/Login_Page.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -6,83 +6,76 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            islogged: false,
-            login_params: {
-                username: "",
-                password: ""
-            }
-        };
-    }
+const Login = () => {
+    const [ state , setState ] = useState({
+        username : "",
+        password : "",
+        isLogged: false
+    })
 
-    handleFormChange = event => {
-        let login_params_new = { ...this.state.login_params };
-        let val = event.target.value;
-        login_params_new[event.target.name] = val;
-        this.setState({
-            login_params: login_params_new
-        });
+    const handleFormChange = event => {
+        const { id , value } = event.target   
+        setState( prevState => ({
+            ...prevState,
+            [id] : value
+        }))
     };
  
-    login = event => {
-        let username = this.state.login_params.username;
-        let password = this.state.login_params.password;
+    const login = event => {
+        let username = state.username;
+        let password = state.password;
         if (username === "admin" && password === "123") {
             localStorage.setItem("token", "T");
             localStorage.setItem("username", username);
-            this.setState({
-                islogged: true
-            });
+            setState( prevState => ({
+                ...prevState,
+                isLogged : true
+            }))
         }
         event.preventDefault();
     };
 
-    render() {
-        if (localStorage.getItem("token")) {
-            return <Redirect to="/" />;
-        }
-        return (
-        <Container component="main" maxWidth="xs">
-            <Avatar/>
-            <Typography component="h1" variant="h5">
-                Sign in
-            </Typography>
-            <form noValidate onSubmit={this.login}>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    label="Username"
-                    name="username"
-                    autoFocus
-                    onChange={this.handleFormChange}
-                />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    onChange={this.handleFormChange}
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                >
-                    Sign In
-                </Button>
-            </form>
-        </Container>
-        );
+    if (localStorage.getItem("token")) {
+        return <Redirect to="/" />;
     }
+    return (
+    <Container component="main" maxWidth="xs">
+        <Avatar/>
+        <Typography component="h1" variant="h5">
+            Sign in
+        </Typography>
+        <form noValidate onSubmit={login}>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Username"
+                id="username"
+                autoFocus
+                onChange={handleFormChange}
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                onChange={handleFormChange}
+            />
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+            >
+                Sign In
+            </Button>
+        </form>
+    </Container>
+    );
 }
 
 export default Login;
